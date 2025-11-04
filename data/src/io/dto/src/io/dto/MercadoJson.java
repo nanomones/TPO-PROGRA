3f4050736fdaceda5550da8b98351e4a58343892
nanomones <monesruiznano@gmail.com>
2025-11-04 00:00:11 -0300

    Delete data/src/io/dto/src/io/dto directory

--- data/src/io/dto/src/io/dto/MercadoJson.java
+++
@@ -1,8 +0,0 @@
-package io.dto;
-
-import java.util.List;
-
-public class MercadoJson {
-    public List<ActivoJson> activos;
-    public List<List<Double>> correlaciones; // matriz n x n (mismo orden que activos)
-}
