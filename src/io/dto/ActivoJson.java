bd778592eda4bf237e84520d8a7c592dd9894535
nanomones <monesruiznano@gmail.com>
2025-11-04 11:57:35 -0300

    Delete src directory

--- src/io/dto/ActivoJson.java
+++
@@ -1,6 +0,0 @@
-package io.dto;
-
-public class ActivoJson {
-    public String ticker, tipo, sector;
-    public double retorno, sigma, montoMin;
-}
--- src/io/dto/MercadoJson.java
+++
@@ -1,8 +0,0 @@
-package io.dto;
-
-import java.util.List;
-
-public class MercadoJson {
-    public List<ActivoJson> activos;          // lista de activos
-    public List<List<Double>> correlaciones;  // matriz n x n de correlaciones
-}
